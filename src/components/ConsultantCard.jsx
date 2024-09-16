acb1e56c9d4f57dcaf79a90a909bee6ffa1707c3
Amandeep Rehal <arehal4@gmail.com>
2024-09-16 16:15:06 -0700

    fix: avatar placeholder

--- src/components/ConsultantCard.jsx
+++ src/components/ConsultantCard.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
-import { Avatar } from "@nextui-org/react";
-
+import AvatarImage from "/public/images/avatar.png";
+import { Avatar } from "@material-tailwind/react";
 
 const renderLocation = (props) => {
   if (props.consultant.defaultAddress) {
@@ -39,23 +39,25 @@ const ConsultantCard = (props) => {
       className="consultant-card flex gap-4 consultant-card__unselected max-h-860 cursor-default border border-tlc-purple rounded-md	"
       id={"consultant-card-" + props.consultant.email}
       key={props.consultant.email}
-    // onClick={(event) => props.radioClickHandler(event, props.consultant)}
     >
-      <div className="flex items-start justify-start hidden lg:block">
-        <Avatar src={`https://randomuser.me/api/portraits/women/${props.number}.jpg`} className="w-8 lg:w-14 rounded-full" />
+      <div className="flex items-start justify-start lg:block">
+        <Avatar
+          src={props.consultant.profileImage ?? AvatarImage.src}
+          className="w-8 lg:w-14 rounded-full"
+        />
       </div>
       <div className="flex flex-col justify-evenly w-full">
-        <Image
+        {/* <Image
           name={props.consultant.displayName}
           className="hidden h-12 w-12 rounded"
           fill={true}
           src={
             props.consultant.profileImage
               ? props.consultant.profileImage
-              : `https://randomuser.me/api/portraits/women/${props.number}.jpg`
+              : AvatarImage
           }
           alt="profile image"
-        />
+        /> */}
         <div className="flex justify-between">
           <div className="consultant-box__name">
             {props.consultant.displayName}
